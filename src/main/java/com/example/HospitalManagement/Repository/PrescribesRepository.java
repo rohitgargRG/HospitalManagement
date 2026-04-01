9e90fe2ffc7fce06070d0d39e13789dcf19d1d8c
Rohit Garg <124335028+rohitgargRG@users.noreply.github.com>
2026-04-01 21:13:18 +0530

    added more functions on Prescription and Medication repo

--- src/main/java/com/example/HospitalManagement/Repository/PrescribesRepository.java
+++ src/main/java/com/example/HospitalManagement/Repository/PrescribesRepository.java
@@ -1,9 +1,10 @@
 package com.example.HospitalManagement.Repository;
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
 import com.example.HospitalManagement.Entity.Prescribes;
 import com.example.HospitalManagement.Entity.PrescribesId;
 
@@ -12,6 +13,7 @@ public interface PrescribesRepository extends JpaRepository<Prescribes , Prescri
 
     List<Prescribes> findByPatient(Integer patient);
     List<Prescribes> findByPhysician(Integer physician);
-    List<Prescribes> findByMedication(Integer medication);
+    // List<Prescribes> findByMedication(Integer medication);
+    Page<Prescribes> findByMedication(Integer medication, Pageable pageable);
     List<Prescribes> findByDose(String dose);  
 }
